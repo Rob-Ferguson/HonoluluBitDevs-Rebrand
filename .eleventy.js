916f706367ecaa115d75ab65a90d8dc05acf0c29
Robby Ferguson <robby@unchained.com>
2023-05-13 23:06:17 -1000

    Trying to add layout aliases to .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,15 @@ const { parse } = require("node-html-parser");
 const htmlMinifier = require("html-minifier");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 
+// adds layout aliases
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addLayoutAlias('event', 'layouts/event.njk');
+  eleventyConfig.addLayoutAlias('resource', 'layouts/resource.njk');
+  eleventyConfig.addLayoutAlias('note', 'layouts/note.njk');
+
+};
+
+
 const { headerToId, namedHeadingsFilter } = require("./src/helpers/utils");
 const {
   userMarkdownSetup,
